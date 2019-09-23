cc0de3d13c796cb7d40eda01d370d5301f492d6b
Jon R. Roma <roma@illinois.edu>
2019-09-23 16:19:07 -0500

    Migrate to Terraform 0.12

--- main.tf
+++ main.tf
@@ -1,36 +1,10 @@
-variable "availability_zone" {
-  description = "Availability zone for volume (NOTE: EC2 instance mounting volume must reside in the same AS as the volume created here"
-}
-
-variable "encrypted" {
-  description = "Encryption"
-  default     = true
-}
-
-variable "size" {
-  description = "The size of the drive in GiBs"
-}
-
-variable "tags" {
-  description = "Mapping of tags to assign to resources"
-  default     = {}
-}
-
 resource "aws_ebs_volume" "default" {
-  availability_zone = "${var.availability_zone}"
-  encrypted         = "${var.encrypted}"
-  size              = "${var.size}"
-  tags              = "${var.tags}"
+  availability_zone = var.availability_zone
+  encrypted         = var.encrypted
+  size              = var.size
+  tags              = var.tags
 
   lifecycle {
     prevent_destroy = true
   }
-}
-
-output "arn" {
-  value = "${aws_ebs_volume.default.arn}"
-}
-
-output "id" {
-  value = "${aws_ebs_volume.default.id}"
 }
