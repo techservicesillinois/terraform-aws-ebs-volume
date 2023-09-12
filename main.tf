c806d5fdd09d9d90bb9d7b2fd335175a4b4d5ff8
Jon R. Roma <jonroma@jonroma.net>
2023-09-11 21:26:20 -0500

    Make various enhancements
    *   Add an output containing the EFS file system ID.
    
    *   Create a "Name" tag consisting of the service and [symbolic]
        name of the volume, separated by a colon, provided both
        values are supplied by the caller.
    
    *   Add support for specifying iops, throughput, and [volume] type;
        all are optional.
    
    *   Update documentation.

--- main.tf
+++ main.tf
@@ -1,8 +1,19 @@
+##### FIXME: Try to simplify this code.
+
+locals {
+  name    = try(var.name, lookup(var.tags, "Name", null, null))
+  service = try(lookup(var.tags, "Service", null), null)
+  tags    = local.name != null && local.service != null ? merge({ Name = format("%s:%s", local.service, local.name) }, var.tags) : var.tags
+}
+
 resource "aws_ebs_volume" "default" {
   availability_zone = var.availability_zone
   encrypted         = var.encrypted
+  iops              = var.iops
   size              = var.size
-  tags              = var.tags
+  tags              = local.tags
+  throughput        = var.throughput
+  type              = var.type
 
   lifecycle {
     prevent_destroy = true
--- variables.tf
+++ variables.tf
@@ -1,5 +1,5 @@
 variable "availability_zone" {
-  description = "Availability zone for volume (NOTE: EC2 instance mounting volume must reside in the same AS as the volume created here"
+  description = "Availability zone for volume (NOTE: EC2 instance mounting volume must reside in the same AZ as the volume created here)"
 }
 
 variable "encrypted" {
@@ -7,12 +7,36 @@ variable "encrypted" {
   default     = true
 }
 
-variable "size" {
-  description = "The size of the drive in GiBs"
+variable "iops" {
+  description = "Amount of IOPS to provision for the disk. Only valid for type of io1, io2 or gp3"
   type        = number
+  default     = null
+}
+
+variable "name" {
+  description = "Name for volume (good choice is basename of mount point)"
+  type        = string
+  default     = null
+}
+
+variable "size" {
+  description = "Size of volume in GiBs"
+  type        = number
+}
+
+variable "throughput" {
+  description = "Throughput supported by volume in MiB/s. Only valid for type gp3"
+  type        = number
+  default     = null
+}
+
+variable "type" {
+  description = "Type of volume"
+  type        = string
 }
 
 variable "tags" {
   description = "Mapping of tags to assign to resources"
   type        = map(string)
+  default     = {}
 }
